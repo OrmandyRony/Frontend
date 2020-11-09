0813b48c6a4122ce3a802ef376b91851228ebf3a
Rony Ortiz <ortizrony699@gmail.com>
2020-11-09 14:59:11 -0600

    prueba 2

--- resenaPelicula.php
+++ resenaPelicula.php
@@ -38,12 +38,14 @@
 	function getParametro(pelicula) {
 		return (window.location.search.match(new RegExp('[?&]' + pelicula + '=([^&]+)')) || [, null])[1];
 	}
-	function decode_utf8 (s) {
-		return decodeURIComponent (escape (s));
-	}
+
 
 </script>
-
+<script>
+function replaceURLWithHTMLLinks(e){return e.replace(/(\(.*?)?\b((?:https?|ftp|file):\/\/[-a-z0-9+&@#\/%?=~_()|!:,.;]*[-a-z0-9+&@#\/%=~_()|])/gi,function(e,r,n){var t="";r=r||"";for(var a=/\(/g;a.exec(r);){var l;(l=/(.*)(\.\).*)/.exec(n)||/(.*)(\).*)/.exec(n))&&(n=l[1],t=l[2]+t)}return r+"<a href='"+n+"' target='_blank' rel='nofollow noopener'>"+n+"</a>"+t})}
+document.addEventListener("DOMContentLoaded",function(event){
+var elm=document.getElementById("text2url");elm.innerHTML=replaceURLWithHTMLLinks(elm.innerHTML);});
+</script>
 <body>
 	<!-- Shell -->
 	<div id="shell">
@@ -119,18 +121,6 @@
 					</div>
 
 <script>
-//Reemplazar al cargar la página
-if (location.hash)
-    location.replace(location.hash.replace(/\+|%20/g, " "));
-
-//Asignar evento cuando se cambie el hash
-window.addEventListener("hashchange", function(event){
-    if (location.hash)
-        location.replace(location.hash.replace(/\+|%20/g, " "));
-
-    //Descomentar para mostrar en consola cuando se dispara
-    //console.debug("Se disparó el evento hashchange: ", event.oldURL, "-->", event.newURL);
-});
 	metodo();
 	mensajes();
 	function metodo(){
@@ -141,11 +131,11 @@ window.addEventListener("hashchange", function(event){
 		xhr.onreadystatechange = (e) => {
 			var peliculas = JSON.parse(xhr.responseText);
 			console.log(peliculas)
-			alert(decode_utf8(getParametro("pelicula")))
+			alert(replaceURLWithHTMLLinks(getParametro("pelicula")))
 			var html = ''
 			for (var i = 0; i < peliculas.length; i++) {
 				
-				if (peliculas[i].pelicula == decode_utf8(getParametro("pelicula"))) {
+				if (peliculas[i].pelicula == getParametro("pelicula")) {
 					alert("entro")
 					html += '<div class="movie"><div class="movie-image">'
 					html += '<a href="#"><span class="play"><span class="name">' + peliculas[i].pelicula + '</span></span><img src="' + peliculas[i].url_imagen + '" alt="movie" /></a></div><div class="rating">'
